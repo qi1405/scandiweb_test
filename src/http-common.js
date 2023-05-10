e0918bd2e155f3f8ff9c2de1764f6bd835421141
dimmy <qi18342@seeu.edu.mk>
2023-05-11 00:17:48 +0200

    updated the url for backend

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL: "http://localhost/scandiweb/product/",
+    baseURL: "https://scandiweb-test-dimmy.000webhostapp.com/product/",
     // baseURL: "https://romedical.herokuapp.com/api",
     headers: {
         "Content-type": "application/json"
