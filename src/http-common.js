3bdebee65b499b3e6cc09ba70e9384a3621e5dfb
jp1405 <85938813+qi1405@users.noreply.github.com>
2023-10-15 17:46:52 +0200

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL: "https://bizairi-001-site1.itempurl.com/scandiweb_3/product/",
+    baseURL: "https://ecoliving2022-001-site1.btempurl.com/scandiweb/product/",
     // baseURL: "https://romedical.herokuapp.com/api",
     headers: {
         "Content-type": "application/json"
